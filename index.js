4db5db64221a89747d110ffd4a666187932596f1
rifarihadts <rifariha.dts@gmail.com>
2020-10-12 10:18:30 +0700

    initial database

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-import express from 'express'
+const express = require('express')
+const hbs = require('hbs')
+const morgan = require('morgan')
+const path = require('path')
 
-const __dirname = path.resolve()
+
+// import express from 'express'
+// import hbs from 'hbs'
+// import path from 'path'
+// import morgan from 'morgan'
+
+
+// const __dirname = path.resolve()
 const app = express()
 
 
-app.send('views', __dirname+'/layouts')
-app.set('view engine', 'hbs')
+app.set('views', __dirname+'/layouts')
+app.set('view engine', 'html')
 app.engine('html', hbs.__express)
 
+app.use(morgan('combined'))
+
+//server static file
+app.use('/assets', express.static(__dirname + '/assets'))
 
 app.get('/', (req, res, next) => {
     res.send({success:true})
 })
+
+app.get('/product', (req, res, next) => {
+    res.render('product')
+})
+
 
 app.use((err, req,res,next) => {
     res.send(err.message)
